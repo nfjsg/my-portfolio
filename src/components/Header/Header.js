4dfb717cc26c7d8202bed3fbeb423cdce8375de4
nfjsg <dasarathan@icloud.com>
2024-01-09 17:36:40 -0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import './Header.css'; // Import your Header-specific styles
 
+const Header = () => {
+  return (
+    <header className="header-container">
+      <h1 className="header-title">Your Name</h1>
+      {/* Add any other header content as needed */}
+    </header>
+  );
+};
+
+export default Header;
