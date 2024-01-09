11e88ea9b0448595d5fac9db32c1faf66bf63611
nfjsg <dasarathan@icloud.com>
2024-01-09 17:34:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,44 @@
 
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import './App.css'; // Import your main CSS file
+import Header from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  const [currentSection, setCurrentSection] = useState('About Me');
+
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Navigation
+          currentSection={currentSection}
+          setCurrentSection={setCurrentSection}
+        />
+
+        <Route path="/" exact>
+          <AboutMe />
+        </Route>
+        <Route path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/resume">
+          <Resume />
+        </Route>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
