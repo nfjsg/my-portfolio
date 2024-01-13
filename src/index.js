6739d62043a81c01a9f171f5204dc1794604184e
nfjsg <dasarathan@icloud.com>
2024-01-13 00:13:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'; // Import your global styles if you have any
+// portfolio-project/src/main.jsx:
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
